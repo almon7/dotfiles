d3a61f345461c565cc97bbb11d8d7b14696dc084
Alberto Montemiglio <71413117+alberto-montemiglio@users.noreply.github.com>
2026-03-01 19:20:10 +0000

    leader-gs toggles git status (added close)

--- nvim/lua/plugins/fugitive.lua
+++ nvim/lua/plugins/fugitive.lua
@@ -7,6 +7,13 @@ return {
 		},
 		config = function()
 			vim.keymap.set('n', '<leader>gs', function()
+				for _, win in ipairs(vim.api.nvim_list_wins()) do
+					local buf = vim.api.nvim_win_get_buf(win)
+					if vim.bo[buf].filetype == 'fugitive' then
+						vim.api.nvim_win_close(win, false)
+						return
+					end
+				end
 				vim.cmd('botright vertical Git')
 				vim.cmd('vertical resize 40')
 			end)
