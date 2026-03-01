d658ae3186c9885f10fc0b894ce535b58cba077d
Alberto Montemiglio <71413117+alberto-montemiglio@users.noreply.github.com>
2026-03-01 22:05:49 +0000

    Add confirmation before closing unsaved files

--- nvim/lua/config/set.lua
+++ nvim/lua/config/set.lua
@@ -21,6 +21,9 @@ vim.opt.incsearch = true -- Show search results as you type
 -- Appearance
 vim.opt.termguicolors = true -- Enable true color support
 
+-- set confirm: ask for confirmation when closing unsaved files
+vim.opt.confirm = true
+
 
 -- Other Settings
 vim.opt.scrolloff = 8 -- Keep 8 lines visible when scrolling
