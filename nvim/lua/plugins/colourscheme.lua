5efdf54b19ce7e090afdfa9b52e73d4a6724505b
Alberto Montemiglio <71413117+alberto-montemiglio@users.noreply.github.com>
2026-03-01 13:31:21 +0000

    Move colourscheme to plugins/

---
+++ nvim/lua/plugins/colourscheme.lua
@@ -0,0 +1,11 @@
+return {
+	"rose-pine/neovim",
+	name = "rose-pine",
+	priority = 1000,
+	lazy = false,
+	config = function()
+		vim.cmd("colorscheme rose-pine")
+		vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+	end
+}
