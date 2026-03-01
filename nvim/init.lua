790d99c8bdda4fb22758de26af7aea6ec26cd282
Alberto Montemiglio <71413117+alberto-montemiglio@users.noreply.github.com>
2026-03-01 15:19:20 +0000

    Add init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,12 @@
-require("almon7")
+-- Load (or install) lazy.nvim
+require("config.lazy")
+
+-- Set remaps and settings
+require("config.set")
+require("config.remap")
+
+-- Load plugins
+require("lazy").setup("plugins")
+
+-- Enable LSP
 vim.lsp.enable("luals")
