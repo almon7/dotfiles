429cb2c5c376def4c6f039d392851791d60b7420
almon7 <71413117+almon7@users.noreply.github.com>
2026-09-04 16:09:31 +0100

    nvim: soften column guide highlight

--- nvim/lua/plugins/column-guide.lua
+++ nvim/lua/plugins/column-guide.lua
@@ -3,5 +3,27 @@ return {
   lazy = false,
   opts = {
     char = "│",
+    highlight = "VirtColumnSubtle",
   },
+  config = function(_, opts)
+    require("virt-column").setup(opts)
+
+    local function set_subtle_highlight()
+      local cursor_line = vim.api.nvim_get_hl(0, { name = "CursorLine", link = false })
+      local normal = vim.api.nvim_get_hl(0, { name = "Normal", link = false })
+      local background = cursor_line.bg or normal.bg
+
+      if background then
+        vim.api.nvim_set_hl(0, "VirtColumnSubtle", { fg = background })
+      else
+        vim.api.nvim_set_hl(0, "VirtColumnSubtle", { link = "NonText" })
+      end
+    end
+
+    vim.api.nvim_create_autocmd("ColorScheme", {
+      group = vim.api.nvim_create_augroup("VirtColumnSubtle", { clear = true }),
+      callback = set_subtle_highlight,
+    })
+    set_subtle_highlight()
+  end,
 }
